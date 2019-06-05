2d84f0ecbfff983d1421340aa39da6ca974c7568
Ziming Ding <uowzd01@hotmail.com>
2019-06-05 22:06:33 +1000

    two pointers is hard

--- 0026_remove_duplicates_from_sorted_array.py
+++ 0026_remove_duplicates_from_sorted_array.py
@@ -11,7 +11,20 @@ class Solution:
                 i += 1
         return len(nums)
 
+    def removeDuplicates(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
 
-nums = {0, 0, 1, 1, 1, 2, 2, 3, 3, 4}
+        i = 0
+        j = i + 1
+        while j < len(nums):
+            if nums[j] != nums[i]:
+                i += 1
+                nums[i] = nums[j]
+            j += 1
+        return i + 1
+
+
+nums = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4]
 print(Solution().removeDuplicates(nums))
 print(nums)
