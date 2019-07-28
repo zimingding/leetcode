d46634ea6c912002f8c8c8db156b468312c60c28
Ziming Ding <uowzd01@hotmail.com>
2019-07-28 18:04:58 +1000

    reverse linked list

---
+++ 0206_reverse_linked_list.py
@@ -0,0 +1,32 @@
+from listnode import ListNode
+
+
+class Solution:
+    def reverseList(self, head: ListNode) -> ListNode:
+        prev = None
+        curr = head
+        while curr:
+            next = curr.next
+            if next:
+                curr.next = prev
+                temp = next.next
+                next.next = curr
+                curr = temp
+                prev = next
+            else:
+                curr.next = prev
+                return curr
+
+        return prev
+
+
+four = ListNode(4)
+three = ListNode(3)
+two = ListNode(2)
+one = ListNode(1)
+one.next = two
+two.next = three
+three.next = four
+
+head = Solution().reverseList(one)
+print(head.val)
