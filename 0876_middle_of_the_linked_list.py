b06bcdbbf71b4220ee36638017538b427185d93c
Ziming Ding <uowzd01@hotmail.com>
2019-08-28 22:52:10 +1000

    middle of the linked list

---
+++ 0876_middle_of_the_linked_list.py
@@ -0,0 +1,11 @@
+from listnode import ListNode
+
+
+class Solution:
+    def middleNode(self, head: ListNode) -> ListNode:
+        slow = head
+        fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        return slow
