7b8002c1b58fedc6280e9a4f9ead2dc74688b1f4
Ziming Ding <uowzd01@hotmail.com>
2019-08-31 15:47:06 +1000

    reverse string

---
+++ 0344_reverse_string.py
@@ -0,0 +1,9 @@
+from typing import List
+
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        for i in range(0, int(len(s)/2)):
+            s[i], s[len(s)-i-1] = s[len(s)-i-1], s[i]
